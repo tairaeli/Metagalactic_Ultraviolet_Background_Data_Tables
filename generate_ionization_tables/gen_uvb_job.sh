6abdb9583e08321c5a60ddfcfe5096611a9dc6c5
Elias Taira <tairaeli@msu.edu>
2025-04-21 14:34:31 -0400

    uploading tables and cloudy scripts

---
+++ generate_ionization_tables/gen_uvb_job.sh
@@ -0,0 +1,20 @@
+#!/bin/bash --login
+
+# A template SLURM script for running CIAOLoop jobs in parallel
+
+#SBATCH -N 4
+#SBATCH --ntasks-per-node 1
+#SBATCH --cpus-per-task 128
+#SBATCH --mem=200G
+#SBATCH -t 40:00:00
+#SBATCH --mail-user=tairaeli@msu.edu
+#SBATCH --mail-type=ALL
+
+# Do any job preamble stuff here; change directories, load modules, etc
+module load Conda/3
+conda activate astro_env
+
+cd /mnt/home/tairaeli/trident_uncertainty/mods/backgrounds/gen_uvb
+
+# Run CIAOLoop in parallel
+srun python ./full_cloudy.py
